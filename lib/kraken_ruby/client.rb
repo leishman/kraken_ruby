364632aaeaf046a9997d436f61adee7a4b49eff5
Alexander Leishman <leishman3@gmail.com>
2014-02-23 16:44:20 -0800

    added private methods, fixed message signature issue.

--- lib/kraken_ruby/client.rb
+++ lib/kraken_ruby/client.rb
@@ -72,12 +72,58 @@ module Kraken
       post_private 'TradeBalance', opts
     end
 
-    def get_public(method, opts={})
-      url = @base_uri + @api_version + '/public/' + method
-      r = self.class.get(url, query: opts)
-      hash = Hashie::Mash.new(JSON.parse(r.body))
-      hash[:result]
+    def open_orders(opts={})
+      post_private 'OpenOrders', opts
     end
+
+    def query_orders(opts={})
+      post_private 'QueryOrders', opts
+    end
+
+    def trade_history(opts={})
+      post_private 'TradesHistory', opts
+    end
+
+    def query_trades(tx_ids, opts={})
+      opts['txid'] = tx_ids
+      post_private 'QueryTrades', opts
+    end
+
+    def open_positions(tx_ids, opts={})
+      opts['txid'] = tx_ids
+      post_private 'OpenPositions', opts
+    end
+
+    def ledgers_info(opts={})
+      post_private 'Ledgers', opts
+    end
+
+    def query_ledgers(ledger_ids, opts={})
+      opts['id'] = ledger_ids
+      post_private 'QueryLedgers', opts
+    end
+
+    def trade_volume(asset_pairs)
+      opts['pair'] = asset_pairs
+      post_private 'TradeVolume', opts
+    end
+
+    #### Private User Trading ####
+
+    def add_order(opts={})
+      required_opts = %w{pair, type, ordertype, volume}
+      opts.keys.each do |key|
+        unless required_opts.include?(1) 
+          raise "Required options, not given. Input must include #{required_opts}"
+        end
+      end
+      post_private 'AddOrder', opts
+    end
+
+    #######################
+    #### Generate Signed ##
+    ##### Post Request ####
+    #######################
 
     def encode_options(opts)
       uri = Addressable::URI.new
@@ -85,46 +131,40 @@ module Kraken
       uri.query
     end
 
-      #### IN PROGRESS #######
-    def post_private(method, opts={})
-
-      # define path
-
-      urlpath = '/' + @api_version + '/private/' + method
-
-      # create nonce
-
-      # opts['nonce'] = Time.now.to_i.to_s.ljust(16,'0')
-      opts['nonce'] = '1393181048417996'
-
-      # encode post data (nonce only)
-
-      post_data = encode_options(opts)
-
-      # decode base 64 key
-
-      key = Base64.decode64(@api_secret)
-
-      # create digest of nonce + post data
-
-      opt_digest = OpenSSL::Digest.new('sha256', opts['nonce'] + post_data).digest
-
-      # create message: urlpath + SHA256(nonce + post_data)
-
-      message = urlpath + opt_digest
-
-      # create auth code
-
-      signature = Base64.encode64("#{OpenSSL::HMAC.digest('sha512', key, message)}").strip
-      
-      headers = {
-        'API-Key' => @api_key,
-        'API-Sign' => signature
-      }
-      
-      url = @base_uri + @api_version + '/private/' + method
-      self.class.post(url, { headers: headers, body: post_data })
+    def generate_nonce
+      Time.now.to_i.to_s.ljust(16,'0')
     end
 
+    def url_path(method)
+      '/' + @api_version + '/private/' + method
+    end
+
+    def generate_message(method, opts, data)
+      digest = OpenSSL::Digest.new('sha256', opts['nonce'] + data).digest
+      url_path(method) + digest
+    end
+
+    def generate_hmac
+      Base64.encode64(OpenSSL::HMAC.digest('sha512', key, message)).split.join # to remove '/n' inserted into signature by HMAc
+    end
+
+    def generate_signature(method, opts={})
+      key = Base64.decode64(@api_secret)
+      message = generate_message(method, opts, post_data)
+      generate_hmac(key, message)
+    end
+
+    def post_private(method, opts={})
+      opts['nonce'] = generate_nonce
+      post_data = encode_options(opts)
+
+      headers = {
+        'API-Key' => @api_key,
+        'API-Sign' => generate_signature(method, opts) 
+      }
+
+      url = @base_uri + url_path(method)
+      self.class.post(url, { headers: headers, body: post_data })
+    end
   end
 end
